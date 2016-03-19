2baa591265ae30e9cf364405405abedfb5317a41
crisoti <cristian.gaspar@gmail.com>
2016-03-18 21:09:09 -0400

    Implemented find operation. Implemented generic Key type. Added Spaceship tests.

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ Utilities
 * C++:
   - bounded_queue.h: cache-efficient implementation using array + composable function applications
   - syncronized_queue.h: synchronized bounded_queued
-  - queue_test.cpp: functional tests for queue 
+  - queue_test.cpp: functional tests for queue
   - queue_test_perf.cpp: performance test comparing against std::queue
   - synchronized_queue_test.cpp: test with multiple threads using the synchronized queue
     clang++ -std=c++11 -pthread synchronized_queue_test.cpp
-  - bst.h: standard binary search tree implementation 
-  - bst_test.cpp: functional test for binary search tree
+  - bst.h: standard binary search tree implementation with a twist: the element type T can specify
+    what Key type is to be used for comparisons. Keys are not stored. They are computed on
+    create/insert/find/delete operations.
+  - bst_test.cpp: functional test for binary search tree: both primitive type (int) and custom type (eg: Spaceship class)
--- cpp/bst.h
+++ cpp/bst.h
@@ -17,21 +17,47 @@ namespace efficient {
 template <class T>
 struct Node {
     Node<T>* children[MAX_CHILD_NUM];
-    T elem;
-    bool hasChildren;
 
-    Node(const T& el) : elem(el), hasChildren(false) {
-        for(auto child: children) {
+    // NOTE: T must implement the conversion-to-key operator:
+    // operator Key() const {
+    //  // return a hash or some unique_id that will serve as key for search operations };
+    //
+    // Usually Key = size_t or Key = string
+    // but could be any type that defines a strict ordering and
+    // operators <,>,== are implemented for the Key type
+    T elem;
+
+    Node(const T& el) : elem(el) {
+        for(auto &child: children) {
             child = nullptr;
         }
     }
 
     Node() = delete;
-    
 };
 
-template<class T>
-void insert(Node<T>** rootPtr, const T& elem, int elemKey) {
+
+// TODO: implement delete-T and rebalancing !!!
+
+template<class T, class Key>
+Node<T>* find(Node<T>* root, const Key& elemKey) {
+    if (!root)
+        return nullptr;
+
+    auto rootKey = (const Key&)(root->elem);
+    if (rootKey == elemKey)
+        return root;
+
+    if ( elemKey > rootKey)
+        return find(root->children[RIGHT], elemKey);
+
+    return find(root->children[LEFT], elemKey);
+}
+
+// Key for the element gets passed explictly to avoid calling the conversion operator
+// on every recursive call
+template<class T, class Key>
+void insert(Node<T>** rootPtr, const T& elem, const Key& elemKey) {
     if (!rootPtr) {
         throw logic_error("BSTinsert: no address to store the root");
     }
@@ -42,7 +68,7 @@ void insert(Node<T>** rootPtr, const T& elem, int elemKey) {
     }
 
     Node<T>* root = *rootPtr;
-    auto rootKey = (int)(root->elem);
+    auto rootKey = (const Key&)(root->elem);
 
     if (elemKey == rootKey) {
         // in tree already
@@ -53,12 +79,12 @@ void insert(Node<T>** rootPtr, const T& elem, int elemKey) {
         if (!root->children[RIGHT]) {
             root->children[RIGHT] = new Node<T>(elem);
             return;
-        }   
+        }
         return insert(&root->children[RIGHT], elem, elemKey);
     }
 
     // Must be Left side
-    if (!root->children[LEFT]) { 
+    if (!root->children[LEFT]) {
         root->children[LEFT] = new Node<T>(elem);
         return;
     }
@@ -66,20 +92,30 @@ void insert(Node<T>** rootPtr, const T& elem, int elemKey) {
 }
 
 // Binary Search Tree
-template<class T>
+template<class T, class Key>
 Node<T>* createBST(const vector<T>& src) {
     bool first = true;
     Node<T>* root = nullptr;
 
     for (const auto &elem: src) {
-        insert(&root, elem, (int) elem);
+        insert(&root, elem, (const Key&) elem);
     }
-
     return root;
 }
 
+
 template<class T>
-void clearTree(Node<T>* root) {
+void clearBST(Node<T>* root) {
+    if (!root) return;
+
+    for (auto child: root->children) {
+        clearBST(child);
+    }
+    delete root;
+}
+
+template<class T>
+void clearBSTNonRecursive(Node<T>* root) {
     if (!root) return;
 
     stack<Node<T>*> s;
@@ -102,16 +138,7 @@ void clearTree(Node<T>* root) {
     }
 }
 
-template<class T>
-void clearTreeRecursive(Node<T>* root) {
-    if (!root) return;
-
-    for (auto child: root->children) {
-        clearTreeRecursive(child);
-    }
-    delete root;
-}
-
+// Depth-first-search that applies function f to each element
 template <class T, class Func>
 void dfs(Node<T>* root, unordered_set<Node<T>*> &visited, const Func& f) {
     if (!root)
@@ -127,6 +154,7 @@ void dfs(Node<T>* root, unordered_set<Node<T>*> &visited, const Func& f) {
     }
 }
 
+// Breadth-first-search that applies function f to each element
 template <class T, class Func>
 void bfs(Node<T>* root, unordered_set<Node<T>*> &visited, const Func& f) {
     if (!root)
@@ -138,7 +166,7 @@ void bfs(Node<T>* root, unordered_set<Node<T>*> &visited, const Func& f) {
     while (!q.empty()) {
         Node<T>* cur = q.front();
         q.pop();
-        
+
         if (visited.find(cur) == visited.end()) {
             visited.insert(cur);
             f(cur->elem);
