4a468f533435c8de4502201c8760a24943cf89d4
Cristian Gaspar <cristian.gaspar@gmail.com>
2023-11-29 10:58:56 -0500

    utils: updated bounded queue with code comments and updated variable names to be more self-explanatory

--- cpp/bounded_queue.h
+++ cpp/bounded_queue.h
@@ -6,90 +6,122 @@
 using namespace std;
 
 namespace efficient {
-    const int32_t INVALID_IDX = -1;
-    const uint32_t MAX_SIZE = 1024 * 1024;
+    const int32_t INVALID_INDEX = -1;
+    const uint32_t QUEUE_MAX_SIZE_DEFAULT = 1024 * 1024;
 
-    // Bounded queue with composable/chainable functional application
+    // Efficient bounded queue implementation with composable/chainable functional application
+    // NOTE: This is not thread-safe. Use SynchronizedQueue for thread-safety.
     template<class T>
     class bounded_queue {
         T* data;
-        int32_t first = INVALID_IDX;
-        int32_t last = INVALID_IDX;
-        uint32_t maxsz = MAX_SIZE;
+        int32_t first = INVALID_INDEX;
+        int32_t last = INVALID_INDEX;
+        uint32_t maxSize = QUEUE_MAX_SIZE_DEFAULT;
 
-        void push_util() {
-            if (size() == maxsz) {
-                // cannot accomodate
-                throw logic_error("BoundedQueue: already at max capacity");
+        /**
+         * Updates the first and last indices to make room for a new element.
+        */
+        void updateIndicesForNewElement() {
+            if (size() == maxSize) {
+                // no room left
+                throw logic_error("BoundedQueue: No room for new element. Already at max capacity");
             }
 
-            if (first == INVALID_IDX) {
-                // previously empty
+            if (first == INVALID_INDEX) {
+                // previously empty queue
                 first = last = 0;
             }
             else {
-                last = (last + 1) % maxsz;
+                // wrap around from buffer end to start if needed
+                last = (last + 1) % maxSize;
             }
         }
 
      public:
-        bounded_queue(const uint32_t max = MAX_SIZE) : maxsz(max) {  data = new T[maxsz]; }
+        bounded_queue(const uint32_t queueMaxSize = QUEUE_MAX_SIZE_DEFAULT) : maxSize(queueMaxSize) {  data = new T[queueMaxSize]; }
         ~bounded_queue() { delete[] data; }
 
         int32_t size() const {
-            if (first == INVALID_IDX || last == INVALID_IDX) {
+            if (first == INVALID_INDEX || last == INVALID_INDEX) {
                 return 0;
             }
 
-            return (last >= first) ? (last - first) + 1 : (maxsz - first) + (last + 1);
+            // 2 cases: no-wrap and wrap-around-buffer-end
+            return (last >= first) ? (last - first) + 1 : (maxSize - first) + (last + 1);
         }
 
+        /**  
+         * Push new element: move-element version
+        */
         void push(T&& elem) {
-            push_util();
+            updateIndicesForNewElement();
             data[last] = move(elem);
         }
 
+        /** 
+         * Push new element: copy-element version 
+        */
         void push(const T& elem) {
-            push_util();
+            updateIndicesForNewElement();
             data[last] = elem;
         }
 
+        /**
+         * Pop oldest element
+        */
         T pop() {
-            int32_t qSize = size();
-            if (qSize == 0) {
-                throw logic_error("Queue: can't pop empty queue!");
+            int32_t queueSize = size();
+            if (queueSize == 0) {
+                throw logic_error("BoundedQueue: Nothing to pop. Empty queue!");
             }
 
-            int32_t idx = first;
-            if (qSize == 1)
+            // Oldest elemnent is the first one.
+            int32_t index = first;
+            if (queueSize == 1)
             {
-                first = last = INVALID_IDX;
+                // To empty queue reset indices to invalid state
+                first = last = INVALID_INDEX;
             }
             else {
-                first = (first + 1) % maxsz;
+                // wrap around from buffer end to start if needed
+                first = (first + 1) % maxSize;
             }
 
-            return move(data[idx]);
+            // Use move semantics to return the element
+            return move(data[index]);
         }
 
+
+        /**
+         * Remove all elements from queue
+        */
         void clear() {
-            while(first != INVALID_IDX) {
+            while(first != INVALID_INDEX) {
                 pop();
             }
         }
 
-        // Application of composable functions
+        /**
+         * Applies given function to each element and returns a reference to the modified queue.
+         * NOTE: If the queue is empty, no exception is thrown. Reference to self is returned to allow chaining to continue.
+         */
         template<class UnaryFunction>
         bounded_queue<T>& operator()(UnaryFunction f) {
             if (size() == 0) {
-                // Empty queue: nothing to apply!
+                // Empty queue: nothing to apply. Return reference to self as is.
                 return *this;
             }
 
-            for (int32_t i = first; i != last; i = (i + 1) % maxsz) {
-                f(data[i]);
+            // Apply function to each element. Wrap around from buffer end to start if needed.
+            // Stop loop when we reach the last element.
+            for (int32_t index = first; index != last; index = (index + 1) % maxSize) {
+                f(data[index]);
             }
+
+            // Apply function to last element
             f(data[last]);
+            
+            // Return reference to self for chaining
             return *this;
         }
     };
--- cpp/queue_test_perf.cpp
+++ cpp/queue_test_perf.cpp
@@ -9,12 +9,12 @@ int main(int argc, char** argv) {
     bounded_queue<int> q;
 
     auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < MAX_SIZE; ++i) {
+    for (int i = 0; i < QUEUE_MAX_SIZE_DEFAULT; ++i) {
         q.push(i);
     }
     auto elapsed = std::chrono::high_resolution_clock::now() - start;
     long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-    cout << "Bounded queue took " << microseconds << " microseconds to push " << MAX_SIZE << " elements" << endl;
+    cout << "Bounded queue took " << microseconds << " microseconds to push " << QUEUE_MAX_SIZE_DEFAULT << " elements" << endl;
 
     start = std::chrono::high_resolution_clock::now();
     q.clear();
@@ -24,12 +24,12 @@ int main(int argc, char** argv) {
 
     std::queue<int> sq;
     start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < MAX_SIZE; ++i) {
+    for (int i = 0; i < QUEUE_MAX_SIZE_DEFAULT; ++i) {
         sq.push(i);
     }
     elapsed = std::chrono::high_resolution_clock::now() - start;
     microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-    cout << "STL queue took " << microseconds << " microseconds to push " << MAX_SIZE << " elements" << endl;
+    cout << "STL queue took " << microseconds << " microseconds to push " << QUEUE_MAX_SIZE_DEFAULT << " elements" << endl;
 
     start = std::chrono::high_resolution_clock::now();
     while (!sq.empty()) {
